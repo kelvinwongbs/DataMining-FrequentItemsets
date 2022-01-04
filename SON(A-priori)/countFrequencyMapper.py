5ab3a256fbbb18eea3d8b48d410989988fca3ebd
kelvinwongbs <61689241+kelvinwongbs@users.noreply.github.com>
2022-01-05 05:06:22 +0800

    Update countFrequencyMapper.py

--- SON(A-priori)/countFrequencyMapper.py
+++ SON(A-priori)/countFrequencyMapper.py
@@ -1,28 +1,21 @@
 import sys
 import itertools
 
-# mapper: read candidate pairs
+# setup: read candidate pairs
 candidatePairs = set()
-for line in sys.stdin:
-    pair, value = line.strip().split('\t', 1)
-    w = pair.split(',')
-    candidatePairs.add((w[0], w[1]))
-# cleanup: count frequency of candidate pairs
+with open("candidate_pairs", "r") as f:
+    for line in f:
+        pair, value = line.strip().split('\t', 1)
+        w = pair.split(',')
+        candidatePairs.add((w[0], w[1]))
+
+# mapper
 N = 0
-with open("shakespeare_basket1", "r") as f:
-    for line in f:
-        N += 1
-        words = line.split()
-        for pair in itertools.combinations(words, 2):
-            sort_pair = tuple(sorted(pair))
-            if  sort_pair in candidatePairs:
-                print('%s,%s\t1' % (sort_pair[0], sort_pair[1]))
-with open("shakespeare_basket2", "r") as f:
-    for line in f:
-        N += 1
-        words = line.split()
-        for pair in itertools.combinations(words, 2):
-            sort_pair = tuple(sorted(pair))
-            if sort_pair in candidatePairs:
-                print('%s,%s\t1' % (sort_pair[0], sort_pair[1]))
+for line in sys.stdin:
+    N += 1
+    words = line.split()
+    for pair in itertools.combinations(words, 2):
+        sort_pair = tuple(sorted(pair))
+        if  sort_pair in candidatePairs:
+            print('%s,%s\t1' % (sort_pair[0], sort_pair[1]))
 print('N\t%d' % N)
